c426fcd7b0249bc283f88017354a942dbcfdd412
Fareed Nathani <160882513+FareedNathani@users.noreply.github.com>
2024-12-20 22:02:55 +0500

    Update firebase.tsx

--- src/app/libs/firebase.tsx
+++ src/app/libs/firebase.tsx
@@ -5,19 +5,17 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import { getAuth } from "firebase/auth"
 const firebaseConfig = {
-  apiKey: "AIzaSyBe3gchiK5iLWIzO2DOyecTUFpyygrBvsU",
-  authDomain: "blogs-3c962.firebaseapp.com",
-  projectId: "blogs-3c962",
-  storageBucket: "blogs-3c962.firebasestorage.app",
-  messagingSenderId: "610991509902",
-  appId: "1:610991509902:web:608b18c42b5bc2724854df",
-  measurementId: "G-M0YQELF4WY"
+  apiKey: "AIzaSyDK-CcQTOJ-eW_mjH4pv7i7r9Dphjunbf0",
+  authDomain: "functional-blog.firebaseapp.com",
+  projectId: "functional-blog",
+  storageBucket: "functional-blog.firebasestorage.app",
+  messagingSenderId: "817486813862",
+  appId: "1:817486813862:web:6b6a063069f3e6dc3ab267",
+  measurementId: "G-Y8TG4Y7Y8Q"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+const analytics = getAnalytics(app);
